9dd177c6de14d48269c2647832489baa4cc9d531
tobiasnf <tobiasnf@gmail.com>
2021-10-09 16:59:47 +0200

    .

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <vector>
 #include ".\include\raylib.h"
 
-#define MAX_COLUMNS 20
-
 using namespace std;
 
 int main(void)
@@ -21,8 +19,13 @@ int main(void)
     camera.fovy = 60.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    std::vector<Vector3> bullets = {};
+    vector<Vector3> bulletPosition = {};
+    Vector3 bulletDimension = {0.3f, 0.3f, 0.3f};
+    vector<Vector3> bulletDirection = {};
     float bulletSpeed = 0.3f;
+    vector<Vector3> enemyPosition = {{-16.0f, 2.5f, 0.0f}, {-16.0f, 8.5f, 0.0f}};
+    vector<Vector3> enemyDimension = {{3.0f, 3.0f, 3.0f}, {6.0f, 6.0f, 6.0f}};
+    Color enemyColor = GREEN;
 
     SetCameraMode(camera, CAMERA_FIRST_PERSON);
 
@@ -30,46 +33,74 @@ int main(void)
 
     while (!WindowShouldClose())
     {
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) /*  (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) */
+        {
+            bulletPosition.push_back({camera.position.x, camera.position.y, camera.position.z});
+            bulletDirection.push_back({camera.target.x - camera.position.x, camera.target.y - camera.position.y, camera.target.z - camera.position.z});
+        };
+
         UpdateCamera(&camera);
-
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
 
+        // Walls and ground
         DrawPlane(Vector3{0.0f, 0.0f, 0.0f}, Vector2{32.0f, 32.0f}, LIGHTGRAY); // Draw ground
-        DrawCube(Vector3{-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, BLUE);           // Draw a blue wall
-        DrawCube(Vector3{16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);            // Draw a green wall
-        DrawCube(Vector3{0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f, GOLD);            // Draw a yellow wall
 
-        // bullets
-        for (int i = 0; i < bullets.size() ;i += 2) {
-            DrawCube(bullets[i], 0.3f, 0.3f, 0.3f, GOLD);
-            bullets[i].x += bullets[i + 1].x * bulletSpeed;
-            bullets[i].y += bullets[i + 1].y * bulletSpeed;
-            bullets[i].z += bullets[i + 1].z * bulletSpeed;
+        // DrawCube(Vector3{-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, BLUE); // Draw a blue wall
+        // DrawCube(Vector3{16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);  // Draw a green wall
+        // DrawCube(Vector3{0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f, GOLD);  // Draw a yellow wall
+
+        // Bullets
+        for (int i = 0; i < bulletPosition.size(); i++)
+        {
+            DrawCube(bulletPosition[i], bulletDimension.x, bulletDimension.y, bulletDimension.z, BLACK);
+            bulletPosition[i].x += bulletDirection[i].x * bulletSpeed;
+            bulletPosition[i].y += bulletDirection[i].y * bulletSpeed;
+            bulletPosition[i].z += bulletDirection[i].z * bulletSpeed;
         };
+
+        // Enemies
+        for (int i = 0; i < enemyPosition.size(); i++)
+        {
+            if (isHitByBullet(enemyPosition[i], enemyDimension[i], bulletPosition))
+                DrawCube(enemyPosition[i], enemyDimension[i].x, enemyDimension[i].y, enemyDimension[i].z, RED);
+            else
+                DrawCube(enemyPosition[i], enemyDimension[i].x, enemyDimension[i].y, enemyDimension[i].z, enemyColor);
+        };
+
         EndMode3D();
 
         // DrawRectangle( 10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
         // DrawRectangleLines( 10, 10, 220, 70, BLUE);
         // DrawText(StringCameraCoordinates, 20, 20, 10, BLACK);
 
+        string test = "Hello World!";
+
         EndDrawing();
 
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            Vector3 startPosition = {camera.position.x, camera.position.y, camera.position.z};
-            Vector3 bulletDirection = {camera.target.x - camera.position.x, camera.target.y - camera.position.y, camera.target.z - camera.position.z};
-            bullets.push_back(startPosition);
-            bullets.push_back(bulletDirection);
-        };
+        //bool xxx = 5 < 10 < 20;
 
-        // string StringCameraCoordinates = to_string(camera.target.x - camera.position.x) + " " + to_string(camera.target.y - camera.position.y) + " " + to_string(camera.target.z - camera.position.z);
-        // if (IsKeyDown(KEY_X)) cout << StringCameraCoordinates /* << endl << xxx */ << endl;
+        if (IsKeyDown(KEY_X)) cout << test << endl;
     }
 
     CloseWindow();
 
     return 0;
 }
+
+bool isHitByBullet(Vector3 enemyPos, Vector3 enemyDime, vector<Vector3> bulletPos)
+{
+    Vector3 enemyCornerOne = {enemyPos.x - enemyDime.x / 2, enemyPos.y - enemyDime.y / 2, enemyPos.z - enemyDime.z / 2};
+    Vector3 enemyCornerTwo = {enemyPos.x + enemyDime.x / 2, enemyPos.y + enemyDime.y / 2, enemyPos.z + enemyDime.z / 2};
+    for (int i = 0; i < bulletPos.size(); i++)
+    {
+        if (enemyCornerOne.x < bulletPos[i].x < enemyCornerTwo.x && enemyCornerOne.y < bulletPos[i].y < enemyCornerTwo.y && enemyCornerOne.z < bulletPos[i].z < enemyCornerTwo.z)
+        {
+            return true;
+        }
+    }
+    return false;
+};
